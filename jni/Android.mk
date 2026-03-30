949f784db2f96945b1aa24768753a934166149ca
invuzt <invuzt@gmail.com>
2026-03-30 21:19:39 +0700

    final: official migration to cakru structure

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := zamera_core_static
-LOCAL_SRC_FILES := libzamera_core.a
+LOCAL_MODULE    := cakru_core_static
+LOCAL_SRC_FILES := libcakru_core.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := hello
 LOCAL_SRC_FILES := hello.c
-LOCAL_STATIC_LIBRARIES := android_native_app_glue zamera_core_static
+LOCAL_STATIC_LIBRARIES := android_native_app_glue cakru_core_static
 LOCAL_LDLIBS    := -llog -lGLESv2 -lEGL
 include $(BUILD_SHARED_LIBRARY)
 
